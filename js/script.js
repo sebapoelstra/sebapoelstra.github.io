10585fae48ae324c9c7b82f761fae8a6dcdee31b
sebapoelstra <124015050+sebapoelstra@users.noreply.github.com>
2023-01-31 00:08:59 -0300

    Add files via upload
    Se sube el proyecto final

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("abrirRazon").onclick = function() {  
+ventanaAbrir() 
+}
+
+
+function ventanaAbrir(URL){
+   window.open("razonplebeya.html","ventana1","width=500,height=800,scrollbars=NO");
+}
+
+
+
